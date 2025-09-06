9d35a7b44114ded0a8ea5f91d04891f71ac1320e
Xiao Li <xiaoli3397@gmail.com>
2025-09-07 02:13:49 +0800

    适配

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,7 @@
 
-save_path=/home/peterjin/debug_cache
+save_path=data/corpus
 
-python download.py --savepath $savepath
+python download.py --save_path $save_path
 
-cat $save_path/part_* > e5_Flat.index
+cat $save_path/part_* > $save_path/e5_Flat.index
+gzip -d $save_path/wiki-18.jsonl.gz
